a9cdf01b57a0893df0a4f14aeedd319adebdc437
eellda <tree500253@gmail.com>
2023-05-26 10:18:35 +0900

    정수 찾기

---
+++ untitled/src/main/java/org/example/lv0/FindInteger.java
@@ -0,0 +1,17 @@
+package org.example.lv0;
+
+public class FindInteger {
+    // 정수 리스트 num_list와 찾으려는 정수 n이 주어질 때,
+    // num_list안에 n이 있으면 1을 없으면 0을 return하도록 solution 함수를 완성해주세요.
+    // 정수 리스트 num_list
+    // 정수 n
+    // num_list안에 n이 있으면 1을 없으면 0
+    public int solution(int[] num_list, int n) {
+
+        for (int i : num_list) {
+            if (i == n) return 1;
+        }
+
+        return 0;
+    }
+}
